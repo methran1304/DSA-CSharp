dc49edf3f5a5f4ea0f8c0ddf06ebac8d2f2605fe
Methran <methran2003@gmail.com>
2025-09-27 04:34:49 +0100

    stack trace recursion impl

--- Algorithms/Recursion/Recursion.cs
+++ Algorithms/Recursion/Recursion.cs
@@ -1,4 +1,6 @@
-﻿namespace Algorithms;
+﻿using System.Xml;
+
+namespace Algorithms;
 
 public static class Recursion
 {
@@ -8,10 +10,24 @@ public static class Recursion
         return n + Sum(n - 1);
     }
 
+    private static int StackTraceVisualisation(int n)
+    {
+        if (n <= 1) throw new Exception("Reached Base Case");
+        return n + StackTraceVisualisation(n - 1);
+    }
+
     public static void Begin()
     {
-        int n = 3;
-        int result = Sum(n);
-        Console.WriteLine(result);
+        int n = 5;
+        try
+        {
+            int result = StackTraceVisualisation(n);
+            Console.WriteLine(result);
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine(ex.StackTrace);
+        }
     }
 }
